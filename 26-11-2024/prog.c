bfed46e19019886c3976013bd0094bdafa1829d3
Paulo Sérgio <pamorimmonico@gmail.com>
2024-11-27 19:05:51 -0300

    aula de ponteiros

--- 26-11-2024/prog.c
+++ 26-11-2024/prog.c
@@ -11,10 +11,23 @@ int main(int argc, char const *argv[])
     printf("*pX = %d\n", *pX);
     printf("&pX = %p\n", &pX);
 
-    int *vec = calloc(sizeof(int), 10);
+    // int **vec = calloc(sizeof(int *), 10);
 
-    for (int i = 0; i < 10; i++)
-        printf("%d ", *(vec + i));
+    // for (int i = 0; i < 10; i++)
+    //     vec[i] = calloc(sizeof(int), 10);
+
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     for (int j = 0; j < 10; j++)
+    //         printf("%d ", vec[i][j]);
+
+    //     printf("\n");
+    // }
+
+    int vec[5] = {1, 2, 3, 4, 5};
+    int *p = vec;
+
+    printf("%d", *(p + 1));
 
     printf("\n");
 
