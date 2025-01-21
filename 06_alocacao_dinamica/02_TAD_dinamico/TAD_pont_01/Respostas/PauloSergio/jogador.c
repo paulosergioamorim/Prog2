f4f24c3e6392624ac04ca4854aa5930394d596b2
Paulo Sérgio <pamorimmonico@gmail.com>
2025-01-21 10:26:39 -0300

    fix tad dinamico 01 (idJogador e idPeca)

--- 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_01/Respostas/PauloSergio/jogador.c
+++ 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_01/Respostas/PauloSergio/jogador.c
@@ -89,44 +89,46 @@ void JogaJogador(tJogador *jogador, tTabuleiro *tabuleiro)
  */
 int VenceuJogador(tJogador *jogador, tTabuleiro *tabuleiro)
 {
-    if (EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 0, 0, jogador->id) &&
-        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 1, 0, jogador->id) &&
-        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 2, 0, jogador->id))
+    int peca = jogador->id == ID_JOGADOR_1 ? PECA_1 : PECA_2;
+
+    if (EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 0, 0, peca) &&
+        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 1, 0, peca) &&
+        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 2, 0, peca))
         return 1;
 
-    if (EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 0, 1, jogador->id) &&
-        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 1, 1, jogador->id) &&
-        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 2, 1, jogador->id))
+    if (EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 0, 1, peca) &&
+        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 1, 1, peca) &&
+        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 2, 1, peca))
         return 1;
 
-    if (EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 0, 2, jogador->id) &&
-        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 1, 2, jogador->id) &&
-        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 2, 2, jogador->id))
+    if (EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 0, 2, peca) &&
+        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 1, 2, peca) &&
+        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 2, 2, peca))
         return 1;
 
-    if (EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 0, 0, jogador->id) &&
-        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 0, 1, jogador->id) &&
-        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 0, 2, jogador->id))
+    if (EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 0, 0, peca) &&
+        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 0, 1, peca) &&
+        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 0, 2, peca))
         return 1;
 
-    if (EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 1, 0, jogador->id) &&
-        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 1, 1, jogador->id) &&
-        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 1, 2, jogador->id))
+    if (EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 1, 0, peca) &&
+        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 1, 1, peca) &&
+        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 1, 2, peca))
         return 1;
 
-    if (EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 2, 0, jogador->id) &&
-        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 2, 1, jogador->id) &&
-        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 2, 2, jogador->id))
+    if (EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 2, 0, peca) &&
+        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 2, 1, peca) &&
+        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 2, 2, peca))
         return 1;
 
-    if (EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 0, 0, jogador->id) &&
-        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 1, 1, jogador->id) &&
-        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 2, 2, jogador->id))
+    if (EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 0, 0, peca) &&
+        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 1, 1, peca) &&
+        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 2, 2, peca))
         return 1;
 
-    if (EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 0, 2, jogador->id) &&
-        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 1, 1, jogador->id) &&
-        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 2, 0, jogador->id))
+    if (EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 0, 2, peca) &&
+        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 1, 1, peca) &&
+        EstaMarcadaPosicaoPecaTabuleiro(tabuleiro, 2, 0, peca))
         return 1;
 
     return 0;
